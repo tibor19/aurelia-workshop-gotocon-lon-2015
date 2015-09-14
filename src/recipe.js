09adfa661b8059719f09af2bc16d530d1eda538d
Tibeiru Covaci <tibi@covaci.se>
2015-09-14 16:21:20 +0100

    Recipe implemented

--- src/recipe.js
+++ src/recipe.js
@@ -1,3 +1,48 @@
-﻿export class Recipe{
+﻿import {inject} from 'aurelia-framework';
+import {join} from 'aurelia-path';
+import {HttpClient} from 'aurelia-http-client';
 
+@inject(HttpClient)
+export class Recipe{
+
+    // static inject = [HttpClient];
+
+    constructor(http){
+        this.http = http;
+        this.recipe = null;
+        this.url = 'http://recipewebapi.azurewebsites.net/api/recipes';
+        this.editMode = false;
+        this.errorMessage = "";
+    }
+
+    activate(params){
+        //join(this.url, params.id)
+        console.log(params.id);
+        return this.http.get(join(this.url, params.id))
+                    .then(response => this.recipe = response.content);
+    }
+
+    editRecipe(){
+        this.editMode = true;
+    }
+
+    saveRecipe(){
+        if(this.recipe == null)
+            return false;
+
+        return this.http.put(join(this.url, this.recipe.recipeID + ""), this.recipe)
+            .then( () => {
+                this.editMode = false;
+                this.errorMessage = "";
+            })
+            .catch(errorMessage => this.errorMessage = errorMessage);
+    }
+
+}
+
+export class ImageValueConverter{
+
+    toView(source){
+        return 'content/images/food/' + source + '.jpg';
+    }
 }
--- src/weekly-menu.js
+++ src/weekly-menu.js
@@ -7,11 +7,12 @@ export class WeeklyMenu {
     constructor(httpClient){
         this.httpClient = httpClient;
         this.recipes = [];
+        this.url = 'data/recipies.json';
     }
 
     activate(){
         let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-        return this.httpClient.get('data/recipies.json')
+        return this.httpClient.get(this.url)
             .then(response => 
                 this.recipes = response.content.map((recipe, index) => {
                     recipe.weekday = weekdays[index % weekdays.length];
